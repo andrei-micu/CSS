141c735d3d665c2edca561a47d00f28793e719a1
Andrei Micu <andrei.micu@info.uaic.ro>
2014-05-21 23:20:16 +0300

    Added assertions for updateApplicant and insertResults methods of DAO

--- ProiectCSS/DatabaseLibrary/DAO.cs
+++ ProiectCSS/DatabaseLibrary/DAO.cs
@@ -26,20 +26,11 @@ namespace DatabaseLibrary
 
         public void insertApplicant(IApplicant applicant)
         {
-            /* assertion for precondition applicant */
-            Debug.Assert(applicant != null, "Applicant should not be null");
-            Debug.Assert(applicant.GeneralAverage >= 1.0 && applicant.GeneralAverage <= 10.0);
-            Debug.Assert(applicant.TestMark >= 1.0 && applicant.TestMark <= 10.0);
-            Debug.Assert(applicant.DomainMark >= 1.0 && applicant.DomainMark <= 10.0);
-            Debug.Assert(applicant.AvgExamen >= 1.0 && applicant.AvgExamen <= 10.0);
-            Debug.Assert(applicant.FatherInitial.Length == 2);
-            Debug.Assert(applicant.FirstName.Length > 2);
-            Debug.Assert(applicant.LastName.Length > 2);
-            Debug.Assert(applicant.Locality.Length > 2);
-            Debug.Assert(applicant.Cnp.Length == 13);
+            /* assertion that the applicant contains proper fields */
+            checkApplicantPreconditions(applicant);
 
-            /* assertion the the path for the file was not modified */
-            Debug.Assert(APPLICANTS_FILE.Equals("applicants.txt") , "The path for the file was changed without permission");
+            /* assertion that the path for the file was not modified */
+            Debug.Assert(APPLICANTS_FILE.Equals("applicants.txt"), "The path for the file was changed without permission");
             checkCreateFile(APPLICANTS_FILE);
             /*assertion that the file should exist at this time*/
             Debug.Assert(File.Exists(APPLICANTS_FILE) == true, "The file should have been created!");
@@ -66,21 +57,19 @@ namespace DatabaseLibrary
 
         public void deleteApplicant(string cnp)
         {
-            /*precondition assertion on CNP format*/
+            /* precondition assertion on CNP format */
             Debug.Assert(cnp.Length == 13, "CNP format not respected!");
 
-            /* assertions for file, it should have the same path and should already exist when deletion is called */
+            /* assertions for file, it should have the same path and should be created if it doesn't exist when deletion is called */
             Debug.Assert(APPLICANTS_FILE.Equals("applicants.txt"), "The path for the file was changed without permission");
+            checkCreateFile(APPLICANTS_FILE);
             Debug.Assert(File.Exists(APPLICANTS_FILE) == true, "The file should have been created!");
 
-            checkCreateFile(APPLICANTS_FILE);
-            
 
             List<string> finalApplicants;
-            
+
             List<string> allApplicants = File.ReadAllLines(APPLICANTS_FILE).ToList();
             /* assertion regarding the length of the applicants list*/
-            Debug.Assert(allApplicants.Count > 0 , "A deletion occurs only if there is at least an applicant");
             Debug.Assert(allApplicants.Count == File.ReadLines(APPLICANTS_FILE).Count(), "The length of the applicants should be the same as lines in the file");
             finalApplicants = allApplicants.Where(x => x.IndexOf(cnp) < 0).ToList();
             if (finalApplicants.Count < allApplicants.Count)
@@ -95,7 +84,17 @@ namespace DatabaseLibrary
 
         public void updateApplicant(string cnp, IApplicant applicant)
         {
+            /* precondition assertion on CNP format */
+            Debug.Assert(cnp.Length == 13, "CNP format not respected!");
+
+            /* assertion that the applicant contains proper fields */
+            checkApplicantPreconditions(applicant);
+
+            /* assertion the the path for the file was not modified */
+            Debug.Assert(APPLICANTS_FILE.Equals("applicants.txt"), "The path for the file was changed without permission");
             checkCreateFile(APPLICANTS_FILE);
+            /* assertion that the file should exist at this time */
+            Debug.Assert(File.Exists(APPLICANTS_FILE) == true, "The file should have been created!");
 
             string[] applicantInfo = { applicant.Cnp, applicant.FirstName, applicant.LastName, applicant.FatherInitial, applicant.City, 
                                        applicant.Locality, applicant.SchoolName, applicant.TestMark.ToString(), applicant.AvgExamen.ToString(), 
@@ -116,11 +115,20 @@ namespace DatabaseLibrary
 
         public void insertResults(IList<IResult> results)
         {
+            /* assertion the the path for the file was not modified */
+            Debug.Assert(RESULTS_FILE.Equals("results.txt"), "The path for the file was changed without permission");
             checkCreateFile(RESULTS_FILE);
+            /* assertion that the file should exist at this time */
+            Debug.Assert(File.Exists(RESULTS_FILE) == true, "The file should have been created!");
 
             foreach (IResult result in results)
             {
+                /* assertion that the result has proper values */
+                Debug.Assert(result != null, "Result entry must not be null");
+                Debug.Assert(result.result != null, "Result result must not be null");
                 IApplicant applicant = result.applicant;
+                checkApplicantPreconditions(applicant);
+
                 string[] applicantInfo = { applicant.Cnp, applicant.FirstName, applicant.LastName, applicant.FatherInitial, applicant.City, 
                                            applicant.Locality, applicant.SchoolName, applicant.TestMark.ToString(), applicant.AvgExamen.ToString(), 
                                            applicant.DomainMark.ToString(), applicant.GeneralAverage.ToString(), result.result};
@@ -222,6 +230,22 @@ namespace DatabaseLibrary
                 fs.Close();
             }
         }
-  
+
+
+        public void checkApplicantPreconditions(IApplicant applicant)
+        {
+            /* assertion for precondition applicant */
+            Debug.Assert(applicant != null, "Applicant should not be null");
+            Debug.Assert(applicant.GeneralAverage >= 1.0 && applicant.GeneralAverage <= 10.0);
+            Debug.Assert(applicant.TestMark >= 1.0 && applicant.TestMark <= 10.0);
+            Debug.Assert(applicant.DomainMark >= 1.0 && applicant.DomainMark <= 10.0);
+            Debug.Assert(applicant.AvgExamen >= 1.0 && applicant.AvgExamen <= 10.0);
+            Debug.Assert(applicant.FatherInitial.Length == 2);
+            Debug.Assert(applicant.FirstName.Length > 2);
+            Debug.Assert(applicant.LastName.Length > 2);
+            Debug.Assert(applicant.Locality.Length > 2);
+            Debug.Assert(applicant.Cnp.Length == 13);
+        }
+
     }
 }
